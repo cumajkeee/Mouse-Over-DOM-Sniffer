0372e70cb02265fe9b0c8326eac20e3d39d882c6
cumajkeee <cumajkeee@gmail.com>
2013-09-05 12:33:39 +0300

    Added code: made singleton, changed to target.attributes

--- js/script.js
+++ js/script.js
@@ -1,36 +1,41 @@
-var target;
-var elementAttr = document.querySelector('.target-info');
-var body = document.getElementsByTagName('body')[0];
-var highlighted = document.querySelector('.highlighted');
+var Sniffer = (function (){
+    var instance = {};
+    var elementAttr = document.querySelector('.target-info');
 
-function removeChildren(node) {
-    var children = node.childNodes;
+    function removeChildren(node) {
+        var children = node.childNodes;
 
-    while(children.length) {
-        node.removeChild(children[0])
+        while(children.length) {
+            node.removeChild(children[0])
+        }
     }
-}
+
+    instance.showElementAttr = function (e) {
+        var target = e.target || event.srcElement;
+        var relatedTarget = e.relatedTarget || e.fromElement;
+
+        removeChildren(elementAttr);
+
+        if (relatedTarget){
+            relatedTarget.style.background="";
+            relatedTarget.style.border="";
+        }
+        target.style.background="#FFFF00";
+        target.style.border="1px solid #FF0000";
+
+        for (var key in target.attributes){
+            var item = document.createElement('div');
+            item.innerHTML = "<b>Attr:</b> " + key + " <b>value:</b>" + target.attributes[key];
+            elementAttr.appendChild(item);
+        }
+        return false;
+    };
+
+    Sniffer = function() {
+        return instance;
+    };
+
+    window.addEventListener('mouseover', instance.showElementAttr, false);
+})();
 
 
-function showElementAttr(e) {
-    var target = e.target || event.srcElement;
-    var relatedTarget = e.relatedTarget || e.fromElement;
-
-    removeChildren(elementAttr);
-
-    if (relatedTarget){
-        relatedTarget.style.background="";
-        relatedTarget.style.border="";
-    }
-    target.style.background="#FFFF00";
-    target.style.border="1px solid #FF0000";
-
-    for (var key in target){
-        var item = document.createElement('div');
-        item.innerHTML = "<b>Attr:</b> " + key + " <b>value:</b>" + target[key];
-        elementAttr.appendChild(item.cloneNode(true))
-    }
-    return false;
-}
-
-body.addEventListener('mouseover', showElementAttr, false);
